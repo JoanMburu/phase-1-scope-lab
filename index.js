c567c1b2baebe22527bdaa431f1a7812fa0093af
JoanMburu <joan.wangui@student.moringaschool.com>
2024-04-07 00:00:17 +0300

    creates scope lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+// Declaring a variable in the global scope
+var customerName = "bob";
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = "not bob";   
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "John";
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "Melvin";
+}
+// upperCaseCustomerName();
+// setBestCustomer();
+
